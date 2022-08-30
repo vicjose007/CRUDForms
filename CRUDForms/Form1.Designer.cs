a6feb66a6210bb0e3d6c808a6f37fa5a69b88ce5
vicjose007 <vicjose007@gmail.com>
2022-08-30 00:18:32 -0400

    Add project files.

---
+++ CRUDForms/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace CRUDForms
+{
+    partial class CRUDForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FirstName = new System.Windows.Forms.TextBox();
+            this.MiddleName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LastName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.PhoneNumber = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.EmailAddress = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.cbClientType = new System.Windows.Forms.ComboBox();
+            this.cbContactType = new System.Windows.Forms.ComboBox();
+            this.CheckSupportStaff = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dgvPeople = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPeople)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(349, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CRUD EXAMPLE: CREATE - READ - UPDATE - DELETE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 348);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name:";
+            // 
+            // FirstName
+            // 
+            this.FirstName.BackColor = System.Drawing.Color.White;
+            this.FirstName.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.FirstName.Location = new System.Drawing.Point(115, 348);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(100, 22);
+            this.FirstName.TabIndex = 2;
+            // 
+            // MiddleName
+            // 
+            this.MiddleName.Location = new System.Drawing.Point(125, 383);
+            this.MiddleName.Name = "MiddleName";
+            this.MiddleName.Size = new System.Drawing.Size(100, 22);
+            this.MiddleName.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 383);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Middle Name:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // LastName
+            // 
+            this.LastName.Location = new System.Drawing.Point(125, 412);
+            this.LastName.Name = "LastName";
+            this.LastName.Size = new System.Drawing.Size(100, 22);
+            this.LastName.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 417);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Last Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 453);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Client Type:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 485);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Contact Type:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(389, 348);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Support Staff:";
+            // 
+            // PhoneNumber
+            // 
+            this.PhoneNumber.Location = new System.Drawing.Point(492, 381);
+            this.PhoneNumber.Name = "PhoneNumber";
+            this.PhoneNumber.Size = new System.Drawing.Size(100, 22);
+            this.PhoneNumber.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(389, 380);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Phone Number:";
+            // 
+            // EmailAddress
+            // 
+            this.EmailAddress.Location = new System.Drawing.Point(492, 412);
+            this.EmailAddress.Name = "EmailAddress";
+            this.EmailAddress.Size = new System.Drawing.Size(100, 22);
+            this.EmailAddress.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(389, 414);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(98, 16);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Email Address:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(30, 293);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(63, 16);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "PEOPLE:";
+            // 
+            // cbClientType
+            // 
+            this.cbClientType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbClientType.FormattingEnabled = true;
+            this.cbClientType.Location = new System.Drawing.Point(125, 445);
+            this.cbClientType.Name = "cbClientType";
+            this.cbClientType.Size = new System.Drawing.Size(121, 24);
+            this.cbClientType.TabIndex = 18;
+            // 
+            // cbContactType
+            // 
+            this.cbContactType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbContactType.FormattingEnabled = true;
+            this.cbContactType.Location = new System.Drawing.Point(125, 482);
+            this.cbContactType.Name = "cbContactType";
+            this.cbContactType.Size = new System.Drawing.Size(121, 24);
+            this.cbContactType.TabIndex = 19;
+            // 
+            // CheckSupportStaff
+            // 
+            this.CheckSupportStaff.AutoSize = true;
+            this.CheckSupportStaff.Location = new System.Drawing.Point(492, 344);
+            this.CheckSupportStaff.Name = "CheckSupportStaff";
+            this.CheckSupportStaff.Size = new System.Drawing.Size(49, 20);
+            this.CheckSupportStaff.TabIndex = 20;
+            this.CheckSupportStaff.Text = "NO";
+            this.CheckSupportStaff.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(270, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Edit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 485);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 537);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 24;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(115, 537);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 25;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(228, 537);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 26;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // dgvPeople
+            // 
+            this.dgvPeople.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvPeople.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPeople.Location = new System.Drawing.Point(21, 58);
+            this.dgvPeople.Name = "dgvPeople";
+            this.dgvPeople.RowHeadersWidth = 51;
+            this.dgvPeople.RowTemplate.Height = 24;
+            this.dgvPeople.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPeople.Size = new System.Drawing.Size(597, 215);
+            this.dgvPeople.TabIndex = 27;
+            this.dgvPeople.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPeople_CellContentClick);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(428, 537);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 28;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(533, 537);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 29;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // CRUDForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 587);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.dgvPeople);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CheckSupportStaff);
+            this.Controls.Add(this.cbContactType);
+            this.Controls.Add(this.cbClientType);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.EmailAddress);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.PhoneNumber);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.LastName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.MiddleName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.FirstName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CRUDForm";
+            this.Text = "People Administration";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPeople)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox FirstName;
+        private System.Windows.Forms.TextBox MiddleName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox LastName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox PhoneNumber;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox EmailAddress;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox cbClientType;
+        private System.Windows.Forms.ComboBox cbContactType;
+        private System.Windows.Forms.CheckBox CheckSupportStaff;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridView dgvPeople;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ CRUDForms/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDForms
+{
+    public partial class CRUDForm : Form
+    {
+        CRUD_ConstSoftwareEntities db = new CRUD_ConstSoftwareEntities();
+
+        List<string> Msg = new List<string>();
+
+        string PeopleId = string.Empty;
+
+        public CRUDForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetContactTypes();
+            GetClientTypes();
+            GetPeople();
+        }
+
+        private void GetPeople()
+        {
+            var people = (from a in db.People
+                         select new
+                         {
+                             a.Id,
+                             Fullname = a.FirstName + " " + a.LastName,
+                             a.PhoneNumber,
+                             a.EmailAddress,
+                             a.Enabled,
+                             a.CreatedDate
+                         }).ToList();
+            dgvPeople.DataSource = people;
+
+        }
+
+        private void GetClientTypes()
+        {
+            var clientTypes = db.ClientTypes.ToList();
+            cbClientType.DataSource = clientTypes;
+            cbClientType.DisplayMember = "Name";
+            cbClientType.ValueMember = "Id";
+        }
+
+        private void GetContactTypes()
+        {
+            var contactTypes = db.ContactTypes.ToList();
+            cbContactType.DataSource = contactTypes;   
+            cbContactType.DisplayMember = "Name";
+            cbContactType.ValueMember = "Id";
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            btnAdd.Enabled = true;
+            btnSave.Enabled = true;
+            btnCancel.Enabled = true;
+        }
+
+        private void SaveForm()
+        {
+            var people = new Person();
+            people.Id = Guid.NewGuid().ToString();
+            people.FirstName = FirstName.Text;
+            people.MiddleName = MiddleName.Text;
+            people.LastName = LastName.Text;
+            people.ClientTypeId = Convert.ToInt32(cbClientType.SelectedValue);
+
+            if(cbContactType.SelectedIndex != 0)
+            {
+                people.ContactTypeId = Convert.ToInt32(cbContactType.SelectedValue);
+            }
+
+            people.SupportStaff = PhoneNumber.Text;
+            people.PhoneNumber = PhoneNumber.Text;
+            people.EmailAddress = EmailAddress.Text;
+            people.Enabled = true;
+            people.CreatedDate = DateTime.Now;
+
+            db.People.Add(people);
+
+            var peopleSaved = db.SaveChanges() > 0;
+        }
+
+        private bool ValidateForm()
+        {
+            Msg = new List<string>();
+
+            bool result = true;
+
+            if (string.IsNullOrEmpty(FirstName.Text))
+            {
+                Msg.Add("First Name Required. ");
+                result = false;
+            }
+            if (string.IsNullOrEmpty(LastName.Text))
+            {
+                Msg.Add("Last Name Required. ");
+                result = false;
+            }
+            if (string.IsNullOrEmpty(PhoneNumber.Text))
+            {
+                Msg.Add("Last Name Required. ");
+                result = false;
+            }
+
+            return result;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (ValidateForm())
+            {
+                SaveForm();
+            }
+            else
+            {
+                string errors = string.Empty;
+                int errorIndex = 1;
+                foreach (var item in Msg)
+                {
+                    errors += $"{errorIndex}. -{item.ToString()}\n";
+                    errorIndex++;
+                }
+                MessageBox.Show(errors, "ERRORS", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+        }
+
+        private void dgvPeople_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            PeopleId = String.Empty;
+
+            if (!string.IsNullOrEmpty(dgvPeople.SelectedRows[0].Cells["Id"].Value.ToString()))
+            {
+                PeopleId = dgvPeople.SelectedRows[0].Cells["Id"].Value.ToString();
+                btnUpdate.Visible = true;
+                btnDelete.Visible = true;
+
+            }
+            else
+            {
+                btnUpdate.Visible = false;
+                btnDelete.Visible = false;
+            }
+        }
+
+        private void GetPeopleById(string peopleId)
+        {
+            DefaultControls();
+            var people = db.People.FirstOrDefault(x => x.Id == peopleId);
+
+            if(people != null)
+            {
+                FirstName.Text = people.FirstName;
+                MiddleName.Text = people.MiddleName;
+                LastName.Text = people.LastName;    
+                PhoneNumber.Text = people.PhoneNumber;
+                EmailAddress.Text = people.EmailAddress;
+
+            }
+        }
+
+        private void DefaultControls()
+        {
+            FirstName.Text = string.Empty;
+            MiddleName.Text = string.Empty;
+            LastName.Text = string.Empty;
+            cbClientType.SelectedIndex = 0;
+            cbContactType.SelectedIndex = 0;
+            CheckSupportStaff.Checked = false;
+            PhoneNumber.Text = string.Empty;
+            EmailAddress.Text = string.Empty;
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            GetPeopleById(PeopleId);
+
+            btnAdd.Enabled = true;
+            btnSave.Enabled = false;
+            btnCancel.Enabled = true;
+            btnUpdate.Visible = false;
+            btnDelete.Visible = false;
+        }
+    }
+}
