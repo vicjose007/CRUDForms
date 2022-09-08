8735b2dff0854ad9ac2bf2fb2725d15f553e0247
vicjose007 <vicjose007@gmail.com>
2022-09-08 18:58:10 -0400

    Added Client Type

---
+++ CRUDForms/Form3.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace CRUDForms
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvClient = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.Name = new System.Windows.Forms.TextBox();
+            this.Description = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClient)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(349, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CRUD EXAMPLE: CREATE - READ - UPDATE - DELETE";
+            // 
+            // dgvClient
+            // 
+            this.dgvClient.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvClient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClient.Location = new System.Drawing.Point(55, 82);
+            this.dgvClient.Name = "dgvClient";
+            this.dgvClient.RowHeadersWidth = 51;
+            this.dgvClient.RowTemplate.Height = 24;
+            this.dgvClient.Size = new System.Drawing.Size(633, 187);
+            this.dgvClient.TabIndex = 2;
+            this.dgvClient.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvClient_CellContentClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Client:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 360);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(52, 408);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Descripcion:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(48, 562);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 26;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(566, 577);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 34;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(469, 577);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 33;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(666, 577);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 32;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(165, 562);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 31;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Name
+            // 
+            this.Name.BackColor = System.Drawing.Color.White;
+            this.Name.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Name.Location = new System.Drawing.Point(125, 360);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(100, 22);
+            this.Name.TabIndex = 35;
+            this.Name.Text = "Form2";
+            // 
+            // Description
+            // 
+            this.Description.Location = new System.Drawing.Point(140, 405);
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(100, 22);
+            this.Description.TabIndex = 36;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 653);
+            this.Controls.Add(this.Description);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvClient);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.Text = "Client Type Administration";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClient)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvClient;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.TextBox Description;
+    }
+}
---
+++ CRUDForms/Form3.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDForms
+{
+    public partial class Form3 : Form
+    {
+        CRUD_ConstSoftwareEntities db = new CRUD_ConstSoftwareEntities();
+
+        List<string> Msg = new List<string>();
+
+        int ClientId = 0;
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            GetClient();
+        }
+
+        private void GetClient()
+        {
+            var client = (from a in db.ClientTypes
+                           select new
+                           {
+                               a.Id,
+                               a.Name,
+                               a.Enabled,
+                               a.CreatedDate
+                           }).ToList();
+            dgvClient.DataSource = client;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            btnAdd.Enabled = true;
+            btnSave.Enabled = true;
+            btnCancel.Enabled = true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (ValidateForm())
+            {
+                SaveForm();
+            }
+            else
+            {
+                string errors = string.Empty;
+                int errorIndex = 1;
+                foreach (var item in Msg)
+                {
+                    errors += $"{errorIndex}. -{item.ToString()}\n";
+                    errorIndex++;
+                }
+                MessageBox.Show(errors, "ERRORS", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+    }
+
+        private void SaveForm()
+        {
+            var client = new ContactType();
+            client.Name = Name.Text;
+            client.Description = Description.Text;
+
+            client.Enabled = true;
+            client.CreatedDate = DateTime.Now;
+
+            db.ContactTypes.Add(client);
+
+            var clientSaved = db.SaveChanges() > 0;
+        }
+
+        private bool ValidateForm()
+        {
+            Msg = new List<string>();
+
+            bool result = true;
+
+            if (string.IsNullOrEmpty(Name.Text))
+            {
+                Msg.Add("Name Required. ");
+                result = false;
+            }
+            if (string.IsNullOrEmpty(Description.Text))
+            {
+                Msg.Add("Description Required. ");
+                result = false;
+            }
+
+            return result;
+        }
+
+        private void dgvClient_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ClientId = 0;
+
+            if (!string.IsNullOrEmpty(dgvClient.SelectedRows[0].Cells["Id"].Value.ToString()))
+            {
+                dgvClient.SelectedRows[0].Cells["Id"].Value.ToString();
+                btnUpdate.Visible = true;
+                btnDelete.Visible = true;
+
+            }
+            else
+            {
+                btnUpdate.Visible = false;
+                btnDelete.Visible = false;
+            }
+        }
+
+        private void GetClientById(int clientId)
+        {
+            DefaultControls();
+            var client = db.ContactTypes.FirstOrDefault(x => x.Id == clientId);
+
+            if (client != null)
+            {
+                Name.Text = client.Name;
+                Description.Text = client.Description;
+
+            }
+        }
+
+        private void DefaultControls()
+        {
+            Name.Text = string.Empty;
+            Description.Text = string.Empty;
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            GetClientById(ClientId);
+
+            btnAdd.Enabled = true;
+            btnSave.Enabled = false;
+            btnCancel.Enabled = true;
+            btnUpdate.Visible = false;
+            btnDelete.Visible = false;
+        }
+    }
+    }
